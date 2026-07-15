4706e81d37c452e588bd9b33f3cf4c98b2459b8d
26nafis <spalanzaninafis@gmail.com>
2026-07-15 14:37:38 +0700

    membuat code delete

--- index.js
+++ index.js
@@ -85,4 +85,34 @@ app.put('/biodata/:id', async (req, res) => {
     }
 });
 //delete
+app.delete('/biodata/:id', async (req, res) => {
+    const { id } = req.params;
 
+    try {
+        const query = `
+            DELETE FROM biodata
+            WHERE id = $1
+            RETURNING *;
+        `;
+
+        const result = await pool.query(query, [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: 'Data tidak ditemukan'
+            });
+        }
+
+        res.status(200).json({
+            message: 'Data berhasil dihapus',
+            data: result.rows[0]
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            error: err.message
+        });
+    }
+});
